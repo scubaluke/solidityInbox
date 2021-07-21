9df4264e8ce7239093d914a9b8994a06ed59bf36
Luke Gartland <lukegartland@gmail.com>
2021-07-21 08:57:51 -1000

    testing set up

--- compile.js
+++ compile.js
@@ -5,6 +5,6 @@ const solc = require('solc');
 const inboxPath =  path.resolve(__dirname, 'contracts', 'Inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf-8')
 
-module.exports = solc.compile(source, 1);
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
 
 
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,28 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
 
+// const  web3 = new Web3(ganache.provider())
+
+class Car {
+    park() {
+        return 'stopped'
+    }
+    drive() {
+        return 'vrooommmm'
+    }
+}
+    let car;
+    beforeEach(() => {
+         car =  new Car()
+
+    })
+
+describe('Car', () => {
+    it('can park', () => {
+        assert.strictEqual(car.park(), 'stopped')
+    })
+    it('can drive', () => {
+        assert.strictEqual(car.drive(), 'vrooommmm')
+    })
+})
